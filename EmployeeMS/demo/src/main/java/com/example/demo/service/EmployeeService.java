906de5ab6c96512e18d2b24a822028be8f8dc3b1
madhumitha-411 <113798713+madhumitha-411@users.noreply.github.com>
2022-12-03 17:38:18 +0530

    Add files via upload

---
+++ EmployeeMS/demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+@Service
+public interface EmployeeService {
+
+	
+		List<Employee> getAllEmployees();
+		void saveEmployee(Employee employee);
+		Employee getEmployeeById(long id);
+		void deleteEmployeeById(long id);
+		Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+	}
+
+
+
